4146ca579922cc497b7304d4999714f83db176ec
53jk1 <kacperbak290@gmail.com>
2021-04-25 22:07:20 +0200

    6.0

---
+++ 15/fetchall.go
@@ -0,0 +1,42 @@
+// Fetchall downloading URL and raporting download time
+// and size of response.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // starting function goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // getting from channel ch
+	}
+	fmt.Printf("%.2fs passed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // sending to channel ch
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("when loading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
---
+++ 16/README.md
@@ -0,0 +1,15 @@
+```
+$ go run writingfile.go
+wrote 5 bytes
+wrote 7 bytes
+wrote 9 bytes
+
+$ cat dat1
+hello
+go
+
+$ cat dat2
+some
+writes
+buffered
+```
